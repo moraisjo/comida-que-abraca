30e018184c6f2b52f3646efb7a4d9dc2a0939508
erica-as <erica.asnt@hotmail.com>
2025-03-30 21:55:23 -0300

    adjustment donation service

--- Codigo/back-end/backend/src/main/java/comidaqueabraca/backend/service/DonationService.java
+++ Codigo/back-end/backend/src/main/java/comidaqueabraca/backend/service/DonationService.java
@@ -3,9 +3,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import comidaqueabraca.backend.entity.DonationEntity;
 import comidaqueabraca.backend.enums.DonationStatus;
 import comidaqueabraca.backend.repository.DonationRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DonationService {
-
     @Autowired
     private DonationRepository donationRepository;
 
