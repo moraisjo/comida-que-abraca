07ffee6817c81065a70e177d8e186ab2f6c6e3a7
erica-as <erica.asnt@hotmail.com>
2025-03-30 22:51:14 -0300

    adjustment campaing service

--- Codigo/back-end/backend/src/main/java/comidaqueabraca/backend/controller/CampaignController.java
+++ Codigo/back-end/backend/src/main/java/comidaqueabraca/backend/controller/CampaignController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
