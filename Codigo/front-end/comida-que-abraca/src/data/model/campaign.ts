cef2052be73d249c16a82380b8f3acc886f728aa
erica-as <erica.asnt@hotmail.com>
2025-04-26 15:21:21 -0300

    frontend-backend integration: search for active campaigns ##16

--- Codigo/front-end/comida-que-abraca/src/data/model/campaign.ts
+++ Codigo/front-end/comida-que-abraca/src/data/model/campaign.ts
@@ -1,5 +1,22 @@
 interface Campaign {
- 
+  id: number;
+  name: string;
+  description: string;
+  address: string;
+  startDate: string; 
+  endDate: string;
+  photoUrl: string;
+  status: "ACTIVE" | "CANCELED" | "FINISHED";
 }
 
-export type { Campaign }
+interface CreateCampaignRequest {
+  name: string;
+  description: string;
+  address: string;
+  startDate: string;
+  endDate: string;
+  photoUrl: string;  
+  status: "ACTIVE"; 
+}
+
+export type { Campaign, CreateCampaignRequest };
--- Codigo/front-end/comida-que-abraca/src/features/OngHomepage/hooks/UseCampaingsService.tsx
+++ Codigo/front-end/comida-que-abraca/src/features/OngHomepage/hooks/UseCampaingsService.tsx
@@ -1,11 +1,30 @@
-import { CampaignRepository } from "../../../data/repository/campaing";
+import { campaignRepository } from "../../../data/repository/campaing";
+import { CreateCampaignRequest, Campaign } from "../../../data/model/campaign";
+import { Response } from "../../../data/model/response";
 
 export const CampaignService = {
-  createCampaign: async (campaignData: any) => {
+  createCampaign: async (
+    campaignData: CreateCampaignRequest
+  ): Promise<Response> => {
     try {
-      return await CampaignRepository.createCampaign(campaignData);
+      return await campaignRepository.createCampaign(campaignData);
     } catch (error) {
       throw new Error("Erro ao criar campanha. Tente novamente.");
     }
   },
+};
+
+export const useCampaignService = () => {
+  const getActiveCampaigns = async (): Promise<Campaign[]> => {
+    try {
+      return await campaignRepository.getActiveCampaigns();
+    } catch (error) {
+      console.error("Erro ao buscar campanhas ativas:", error);
+      return [];
+    }
+  };
+
+  return {
+    getActiveCampaigns,
+  };
 };
