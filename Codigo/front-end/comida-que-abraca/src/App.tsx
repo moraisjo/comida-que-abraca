03b7c4e916093391a5cc04259ca3a18ead1a38aa
erica-as <erica.asnt@hotmail.com>
2025-05-27 01:44:14 -0300

    remoção de opção header

--- Codigo/front-end/comida-que-abraca/src/App.tsx
+++ Codigo/front-end/comida-que-abraca/src/App.tsx
@@ -1,11 +1,15 @@
 import "./shared/theme/global.css";
 
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import RankingPage from "./features/RankingPage/RankingPage";
 import CampanhasPage from "./features/ManageCampaigntPage/components/CampaignPages";
 import DonationPage from "./features/ManageDonationsPage/DonationPage";
 import PartnerPage from "./features/partner/PartnerPage";
-import AvailableCampaignsPage from "./features/AvailableCampaigns/AvailableCampaignsPage";
 import LoginPage from "./features/LoginPage/LoginPage";
 import OngHomepage from "./features/OngHomepage/OngHomepage";
 import SignUpPage from "./features/SignUpPage/SignUpPage";
@@ -40,7 +44,6 @@ function App() {
           <Route path="/doacoes" element={<DonationPage />} />
           <Route path="/cadastro-parceiro" element={<PartnerPage />} />
           <Route path="/cadastro" element={<SignUpPage />} />
-          <Route path="/campanhas-disponiveis" element={<AvailableCampaignsPage />} />
           <Route path="/relatorios" element={<ReportPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/lgpd" element={<LgpdRouteWrapper />} />
